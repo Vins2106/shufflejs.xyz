bb2235123dc46918c99e83ae3ed45e3c88b1e31e
Glitch (hello-express) <none>
2021-02-25 08:03:23 +0000

    🍲🎳 Checkpoint ./src/commands/help/help.js:25331590/47 ./src/commands/help/module.json:25331590/1 ./src/commands/helps/help.js:25331590/462

--- src/commands/helps/help.js
+++ src/commands/helps/help.js
@@ -3,25 +3,33 @@ exports.run = async (message, client, args, config) => {
   if (client.modules.get(args[0])) {
     let mod = client.modules.get(args[0]);
     
-    const cmdE = new client.embed()
+    const modE = new client.embed()
     .setAuthor(`${mod.emoji} - ${mod.name}`)
     .setColor(config.embed)
     .setFooter("UwU!")
     
     mod.cmds.forEach(cmd => {
-      cmdE.addField(`${cmd.name} - ${cmd.cooldown}`, `\n\n**[${cmd.aliases.join(", ")}]**\n**${cmd.description}**`, true)
+      modE.addField(`${cmd.name} - ${cmd.cooldown}s`, `\n\n**[${cmd.aliases.join(", ")}]**\n**${cmd.description}**`, true)
     });
     
-    return message.channel.send(cmdE)
+    return message.channel.send(modE)
   } else if(client.commands.has(args[0]) || client.commands.get(client.aliases.get(args[0]))) {
-    let cmd 
+    let cmd = client.commands.has(args[0]) || client.commands.get(client.aliases.get(args[0]));
+    
+    let cmdE = new client.embed()
+    .setAuthor(`${cmd.name} - ${cmd.cooldown}s`)
+    .setDescription(`**${cmd.description}**`)
+    .addField(`Aliaes`, `**[${cmd.aliases.join(", ")}]**`)
+    .setColor(config.color)
+    
+    return message.channel.send(cmdE)
   }
   
   const helpE = new client.embed()
   .setAuthor(client.user.username + "", client.user.displayAvatarURL())
   .setDescription("Holla!")
   .setColor(config.embed)
-  .setFooter(`${config.prefix}help [category]`)
+  .setFooter(`${config.prefix}help [category] / ${config.prefix}help [command]`)
   
   client.modules.forEach(cat => {
     helpE.addField(`${cat.emoji} - ${cat.name}`, `**${cat.description}**`, true)
