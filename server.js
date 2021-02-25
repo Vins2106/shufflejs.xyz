accdd0afa2ac4138b2fe29b2226085dd83681d98
Glitch (hello-express) <none>
2021-02-25 10:00:18 +0000

    📈🐵 Checkpoint ./server.js:25331590/2486

--- server.js
+++ server.js
@@ -16,15 +16,6 @@ const fs = require("fs");
 
 client.embed = Discord.MessageEmbed;
 
-client.youtube = youtube
-client.Util = Util
-
-module.exports = {Util, music, ytdl, client, config, Discord}
-
-const { handleVideo, play } = require("./function.js")
-
-client.handleVideo = handleVideo;
-client.play = play
 
 client.login(config.token)
 
@@ -58,3 +49,71 @@ client.on("message", async message => {
 })
 
 // events akhir
+
+// function
+async function handleVideo(video, message, voiceChannel, playlist = false) {
+    const serverQueue = music.get(message.guild.id);
+    const song = {
+        id: video.id,
+        title: Util.escapeMarkdown(video.title),
+        url: `https://www.youtube.com/watch?v=${video.id}`,
+        thumbnail: ``
+    };
+    if (!serverQueue) {
+        const queueConstruct = {
+            textChannel: message.channel,
+            voiceChannel: voiceChannel,
+            connection: null,
+            songs: [],
+            volume: 100,
+            playing: true,
+            loop: false
+        };
+        music.set(message.guild.id, queueConstruct);
+        queueConstruct.songs.push(song);
+
+        try {
+            var connection = await voiceChannel.join();
+            queueConstruct.connection = connection;
+            play(message.guild, queueConstruct.songs[0]);
+        } catch (error) {
+            console.error(`[ERROR] I could not join the voice channel, because: ${error}`);
+            music.delete(message.guild.id);
+            return message.channel.send(`I could not join the voice channel, because: **\`${error}\`**`);
+        }
+    } else {
+        serverQueue.songs.push(song);
+        if (playlist) return;
+        else return message.channel.send(`<:yes:591629527571234819>  **|**  **\`${song.title}\`** has been added to the queue`);
+    }
+    return;
+}
+
+function play(guild, song) {
+    const serverQueue = music.get(guild.id);
+
+    if (!song) {
+        serverQueue.voiceChannel.leave();
+        return music.delete(guild.id);
+    }
+
+    const dispatcher = serverQueue.connection.play(ytdl(song.url))
+        .on("finish", () => {
+            const shiffed = serverQueue.songs.shift();
+            if (serverQueue.loop === true) {
+                serverQueue.songs.push(shiffed);
+            };
+            play(guild, serverQueue.songs[0]);
+        })
+        .on("error", error => console.error(error));
+    dispatcher.setVolume(serverQueue.volume / 100);
+
+    serverQueue.textChannel.send({
+        embed: {
+            color: "BLUE",
+            description: `🎶  **|**  Start Playing: **\`${song.title}\`**`
+        }
+    }).then(m => m.delete({
+      timeout: 5000
+    }));
+}
