6d875572b71093a2894e13166a9363eb6f5562af
Glitch (hello-express) <none>
2021-02-26 13:19:33 +0000

    ☺️👓 Checkpoint ./src/commands/music/queue.js:25331590/972

--- src/commands/music/queue.js
+++ src/commands/music/queue.js
@@ -1,8 +1,25 @@
+const { MessageEmbed } = require('discord.js');
+
 exports.run = async (message, client, args, music, config) => {
   
   if (!music.get(message.guild.id)) return message.channel.send(`This server do not have current music :<`);
   
-  const queueE = new Discord
+  let queue = music.get(message.guild.id);
+  
+  let index = 0;
+  
+  let songs = queue.songs.slice(0, 10);
+  
+  const queueE = new MessageEmbed()
+  .setAuthor(message.guild.name + " queue! c:", message.guild.iconURL())
+  .setDescription(`Current: **${queue.songs[0].title}** - **${queue.songs[0].duration.hours}** : **${queue.songs[0].duration.minutes}** : **${queue.songs[0].duration.seconds}**
+  
+  ${songs.map(song => `**[${++index}.]** - **${song.title}** - **${song.duration.hours}** : **${song.duration.minutes}** : **${song.duration.seconds}**`).join("\n")}`)
+  .setImage(queue.songs[0].thumbnail.url)
+  .setColor(config.embed)
+  .setFooter(`This songs from 1 - 10 in queue`)
+  
+  message.channel.send(queueE)
   
 }
 
