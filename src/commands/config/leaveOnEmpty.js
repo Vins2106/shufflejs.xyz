98ca318d23d81db2954f2759af7d81dcad0f4eef
Glitch (hello-express) <none>
2021-02-26 12:33:58 +0000

    👢⚓️ Checkpoint ./src/commands/config/leaveOnEmpty.js:25331590/1005

--- src/commands/config/leaveOnEmpty.js
+++ src/commands/config/leaveOnEmpty.js
@@ -7,7 +7,17 @@ exports.run = async (message, client, args, config) => {
     return message.channel.send(`The leaveOnEmpty plugin for this server is **${client.guildConfig.get(`config.${message.guild.id}.leaveOnEmpty`) ? "Enable" : "Disable"}**`)
   }
   
-  if (!query === "true" || !query) {}
+  if (query === "enable" || query === "on" || query === "true") {
+    
+  } else if (query === "disable" || query === "off" || query === "false") {
+    
+  } else {
+    return message.channel.send(`>:c
+enable:
+${config.prefix}leave-on-empty enable
+${config.prefix}leave-on-empty on
+${config.prefix}leave-on-empty`)
+  }
   
 }
 
