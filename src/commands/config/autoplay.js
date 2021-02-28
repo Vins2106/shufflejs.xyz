5412a97d883d1cd4367e8d579d95f780ed9379c1
Glitch (hello-express) <none>
2021-02-28 10:36:25 +0000

    💾💇 Checkpoint ./src/commands/config/autoplay.js:25331590/1260

--- src/commands/config/autoplay.js
+++ src/commands/config/autoplay.js
@@ -2,9 +2,45 @@ exports.run = async (message, client, args, music, config, handleVideo, play, yo
   
   if (!message.member.hasPermission("MANAGE_GUILD")) return message.channel.send(`Hey, please contact **${message.guild.id}** admin to give you **Manage Guild** permission!`);
   
+  if (!music.get(message.guild.id)) return message.channel.send(`Please play a music first!`)
+  
   let msg = await message.channel.send(`React with ↪️ for **turn on or turn off autoplay**`);
   
-  let filter = (reaction, user) => user.id !== client.user.id && }
+  msg.react("↪️");
+  
+  let filter = (reaction, user) => user.id !== client.user.id && user.id == message.author.id;
+  
+  let collector = msg.createReactionCollector(filter, {time: 30000});
+  
+  collector.on("collect", async (reaction, user) => {
+    
+    switch (reaction.emoji.name) {
+        
+      case "↪️":
+        
+        reaction.users.remove(user);
+        
+        let _enable = music.get(message.guild.id).shuffle ? true : false;
+        let _conf = _enable;
+        
+        
+        if (_enable) {
+          _conf = false;
+        } else if (!_enable) {
+          _conf = true;
+        }
+        
+        music.get(message.guild.id).shuffle = _conf;
+        
+        msg.delete();
+        
+        return message.channel.send(`Autoplay has been turn **${music.get(message.guild.id).shuffle ? "on" : "off"}**!`);
+        
+        break;
+        
+    }
+    
+  })
   
 }
 
