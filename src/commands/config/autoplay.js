54a481f18696566e36a22ca6b911c374c27bad21
Glitch (hello-express) <none>
2021-02-28 09:04:37 +0000

    🌸💸 Checkpoint ./src/commands/config/autoplay.js:25331590/269

---
+++ src/commands/config/autoplay.js
@@ -0,0 +1,10 @@
+exports.run = async (message, client, args, music, config, handleVideo, play, youtube, url) => {
+
+}
+
+exports.config = {
+  name: "autoplay",
+  description: "Turn on / off server autoplay",
+  aliases: ["ap"],
+  cooldown: 10
+}
