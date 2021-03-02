4d177a2b49175b7cefa39bf30e83fcf1ff5e393f
Glitch (magnificent-available-justice) <none>
2021-03-02 14:59:47 +0000

    💿⛑ Checkpoint ./src/commands/voice/reject.js:25331590/664

--- src/commands/voice/reject.js
+++ src/commands/voice/reject.js
@@ -11,7 +11,23 @@ exports.run = async (message, client, args, music, config, handleVideo, play, yo
   
   let getUserVC = await db.get(`jfc.${message.author.id}.voice`);
   
-  if (voiceChannel.id !== getUserVC.id) return message.channel.send(`This is not your own voice channel, claim this voice to reject user!`)
+  if (voiceChannel.id !== getUserVC.id) return message.channel.send(`This is not your own voice channel, claim this voice to reject user!`);
+  
+  let user = message.mentions.users.first() || client.users.cache.get(args[0]) || await client.users.fetch();
+  if (!user) return message.channel.send(`Please mention or give user id!`)
+  
+  let getOwn = await db.get(`ownerJFC.${voiceChannel.id}`);
+  
+  if (user.id == getOwn.id) return message.channel.send(`You cant reject voice owner!`)
+  
+  voiceChannel.overwritePermissions([
+    {
+      id: user.id,
+      deny: ['VIEW_CHANNEL', 'CONNECT']
+    }
+  ]);
+  
+  
   
 }
 
