a71ba46a2bcd9081de3e2acc364c0d38c70ffa89
Glitch (hello-express) <none>
2021-03-02 06:43:49 +0000

    🍬🚎 Checkpoint ./src/commands/config/jointocreate.js:25331590/1474

--- src/commands/config/jointocreate.js
+++ src/commands/config/jointocreate.js
@@ -1,11 +1,23 @@
+const db = require("quick.db");
+
 exports.run = async (message, client, args, music, config, handleVideo, play, youtube, url) => {
 
   if (!message.member.hasPermission("MANAGE_GUILD")) return message.channel.send(`Sorry, you need **Manage Guild** permission`);
   
-  if (!args[0]) {
-    return message.channel.send(`Missing argument:
-**${config.prefix}join-to-create <#channel>**`)
-  }
+    message.guild.channels.create('JTC', {
+      type: 'category'
+    }).then(c => {
+      c.setPosition(0)
+      message.guild.channels.create("➕ Join To Create", {
+        type: 'voice',
+        userLimit: 1
+      }).then(c1 => {
+        c1.setParent(c.id)
+        db.set(`jfc.${message.guild.id}`, c1.id)
+        return message.channel.send(`Succesfully create join-to-create voice channel! (**${c1.name}**)`)
+      })
+    });
+  
   
 }
 
