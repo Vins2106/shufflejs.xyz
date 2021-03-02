55ba198324fcd0f12bcf22101cc1973ba4a6ee6b
Glitch (magnificent-available-justice) <none>
2021-03-02 09:22:09 +0000

    👨‍❤️‍💋‍👨🏣 Checkpoint ./src/commands/voice/lock.js:25331590/232

---
+++ src/commands/voice/lock.js
@@ -0,0 +1,10 @@
+exports.run = async (message, client, args, music, config, handleVideo, play, youtube, url) => {
+
+}
+
+exports.config = {
+  name: "",
+  description: "Lock your voice channel",
+  aliases: ["lock-vc"],
+  cooldown: 10
+}
