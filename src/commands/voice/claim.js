f59a1547615e88ade24aa05b7aca11b9d06e3650
Glitch (magnificent-available-justice) <none>
2021-03-02 08:32:33 +0000

    🏌🎙 Checkpoint ./src/commands/voice/claim.js:25331590/932

--- src/commands/voice/claim.js
+++ src/commands/voice/claim.js
@@ -1,8 +1,20 @@
+let db = require("quick.db");
+
 exports.run = async (message, client, args, music, config, handleVideo, play, youtube, url) => {
 
-  let jtc = 
+  let jtc = db.get(`jfc.${message.guild.id}`);
   
-  if (!message.member.voice.channel) return message.channel.send(`Please join`)
+  if (!jtc) return message.channel.send(`Please tell the admin to setup join-to-create first`);
+  
+  let voiceChannel = message.member.voice.channel;
+  
+  let jtcChl = client.channels.cache.get(jtc) || await client.channels.fetch(jtc);
+  
+  if (!voiceChannel) return message.channel.send(`Please join **${jtcChl.name}** to create your own voice channel!`);
+  
+  let getChl = await db.get(`tempvoicechannel_${message.guild.id}_${voiceChannel.id}`);
+  
+  if (!getChl) return message.channel.send(``)
   
 }
 
