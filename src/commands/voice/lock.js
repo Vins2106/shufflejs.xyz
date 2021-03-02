6e6f4452cc70159af1648ab581049ff169cedab3
Glitch (magnificent-available-justice) <none>
2021-03-02 09:33:32 +0000

    🌝🍇 Checkpoint ./src/commands/voice/lock.js:25331590/576

--- src/commands/voice/lock.js
+++ src/commands/voice/lock.js
@@ -1,9 +1,18 @@
-exports.run = async (message, client, args, music, config, handleVideo, play, youtube, url) => {
+const db = require("quick.db");
 
+exports.run = async (message, client, args, music, config, handleVideo, play, youtube, url) => {
+  
+  let jtc = await db.get(`jfc.${message.guild.id}`);
+  if (!jtc) return message.channel.send(`This server do not setup join-to-create!`);
+  
+  let voiceChannel = message.member.voice.channel;
+  
+  if (!voiceChannel) return message.channel.send(`Please join **${message.guild.channels.cache.get(jtc).name}** to create your own voice`)
+  
 }
 
 exports.config = {
-  name: "",
+  name: "lock",
   description: "Lock your voice channel",
   aliases: ["lock-vc"],
   cooldown: 10
