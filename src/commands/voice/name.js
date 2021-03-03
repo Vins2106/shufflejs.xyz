caaf692cc1a319abf5ab12dc298ce735c00bcef2
Glitch (magnificent-available-justice) <none>
2021-03-03 12:31:23 +0000

    🏩🚝 Checkpoint ./src/commands/voice/name.js:25331590/3518

--- src/commands/voice/name.js
+++ src/commands/voice/name.js
@@ -1,9 +1,27 @@
 const db = require("quick.db");
+let ms = require("parse-ms");
 
 exports.run = async (message, client, args, music, config, handleVideo, play, youtube, url) => {
   
   let jtc = await db.get(`jfc.${message.guild.id}`);
   if (!jtc) return message.channel.send(`This server do not setup join-to-create!`);
+  
+  
+    let pad_zero = num => (num < 10 ? '0' : '') + num;
+    let cooldown = 100000;
+    let lastCreate = db.get(`lastCNM.${message.author.id}`);
+        
+        if (lastCreate !== null && cooldown - (Date.now() - lastCreate) > 0) {
+            let timeObj = ms(cooldown - (Date.now() - lastCreate));
+
+       
+                let mins = pad_zero(timeObj.minutes).padStart(2, "0"),
+                secs = pad_zero(timeObj.seconds).padStart(2, "0");
+
+            let finalTime = `**${mins}:${secs}**`;
+            return message.channel.send(`You cant change your voice channel name after ${finalTime}`);
+          
+        } else { 
   
   let voiceChannel = message.member.voice.channel;
   if (!voiceChannel) return message.channel.send(`Please join **${client.channels.cache.get(jtc).name}** first! to create your own voice channel!`);
@@ -28,8 +46,13 @@ exports.run = async (message, client, args, music, config, handleVideo, play, yo
     name: newName
   });
   
+          db.set(`lastCNM.${message.author.id}`, Date.now())
+          
   return message.channel.send(`Your voice channel name has been set to **${newName}**`)
   
+          
+          
+        }  
 }
 
 exports.config = {
